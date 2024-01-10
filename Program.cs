f56d7bc0361c774e6f7163140ff049184a51f846
ranieriiuri <iuriranierioliveira@gmail.com>
2024-01-10 15:21:36 -0300

    dioAvanadeDecolaTechBootcamp 'Criando um sistema e abstraindo um celular com POO' finished

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// DONE: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:\n");
+Smartphone nokia = new Nokia("8298188-9670", "C20", "000000001010", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Nubank");
+
+Console.WriteLine("\nSmatphone Iphone:\n");
+Smartphone iphone = new Iphone("8199450-3223", "XR", "000000001020",64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Coffeely");
+
